18d933c3f4fad31f5c8e1b4c1f38a2077e62d196
Jonathan Chung <jono.chung@gmail.com>
2021-06-26 14:53:07 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     description='Platform domain OpenAI Gym environment',
     author='Craig Bester',
-    packages=find_packages(include=['gym_platform', 'gym_platform.*'],
+    packages=find_packages(include=['gym_platform', 'gym_platform.*']),
     include_package_data=True, # include asset files. TODO: specify specific files?
     install_requires=['gym',
                       'pygame', #'pygame>=1.9.3'
